04104250898a6c635046771d977519577c125b8e
Salvador Jimenez <salvad.j.f@gmail.com>
2023-11-09 20:34:10 +0100

    9 de Noviembre Entorno Servidor Antes del Breeze

--- tiendalaravel2324/routes/web.php
+++ tiendalaravel2324/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Prueba para demostrar como funciona el enrutador
+Route::get('/salva', function () {
+    return 'Hola mundo';
+});
+*/
+
+Route::get('/categorias', function () {
+    return view('');
+});
