c7aa724a8ed14024f48920e1db7000ab645e7ba9
Salvador Jimenez <salvad.j.f@gmail.com>
2023-11-13 21:17:37 +0100

    13 de Noviembre, Entorno Servidor.

---
+++ tiendalaravel2324/resources/views/categorias/index.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+
+    @foreach ($categorias as $categoria)
+        {{ $categoria }}
+    @enforeach
+</x-guest-layout>
--- tiendalaravel2324/resources/views/salva.blade.php
+++ tiendalaravel2324/resources/views/salva.blade.php
@@ -1,4 +1,7 @@
 <x-guest-layout>
 Hola
+<x-application-logo />
+<x-danger-button>Hola </x-danger-button>
+<x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
 </x-guest-layout>
 
