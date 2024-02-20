eb754dbfa0a104a921c249aa443db7d7fe97acec
Salvador Jimenez <salvad.j.f@gmail.com>
2024-02-20 18:45:35 +0100

    Modelo Vuelo Creado

---
+++ Examenes Antiguos/e2018/database/migrations/2024_02_20_163435_create_vuelos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vuelos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo', 6)->unique();
+            $table->string('origen', 3);
+            $table->string('destino', 3);
+            $table->string('compañia aerea');
+            $table->dateTime('salida');
+            $table->dateTime('llegada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vuelos');
+    }
+};
