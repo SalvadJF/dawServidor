92de1460c66cc44dde65b1b35ff57ed252100c0f
Salvador Jimenez <salvad.j.f@gmail.com>
2024-02-20 19:35:13 +0100

    arreglo

--- Examenes Antiguos/e2018/database/migrations/2024_02_20_181118_create_aeropuertos_table.php
+++ Examenes Antiguos/e2018/database/migrations/2024_02_20_181118_create_aeropuertos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('aeropuertos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->morphs('codigo', 3);
+            $table->string('codigo')->unique();
             $table->timestamps();
         });
     }
